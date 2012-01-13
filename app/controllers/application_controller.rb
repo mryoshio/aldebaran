566f8d48e440344406e5ea8422a3339259e05718
mryoshio <yoshiokaas@gmail.com>
2012-01-14 02:23:08 +0900

    impl search function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
+require 'rubygems'
+require 'active_cmis'
+
+
 class ApplicationController < ActionController::Base
+  helper :all
   protect_from_forgery
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'active_cmis'
 
 CmisSample::Application.configure do
@@ -30,8 +31,35 @@ CmisSample::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-
+  # setup CMIS repository
   $cmis_config = Hash.new
-  $cmis_config['repo_id'] = '9f5884b6-cb46-40a6-9eed-0e94bd02c42f'
+  $cmis_config["repo_id"] = "acae41c9-1717-492e-8b0a-410d888ff423"
+  alfresco = ActiveCMIS::Server.new("http://127.0.0.1:8080/alfresco/service/cmis")
+  alfresco.authenticate(:basic, 'admin', 'admin')
+  $repo = alfresco.repository($cmis_config['repo_id'])
+  $icons = {
+    "application/x-javascript" =>"/alf_images/js.gif",
+    "text/plain" => "/alf_images/text-file-32.png",
+    "text/html" => "/alf_images/html.gif",
+    "application/msword" => "/alf_images/doc.gif",
+    "text/xml" => "/alf_images/xml.gif",
+    "image/gif" => "/alf_images/gif.gif",
+    "image/jpeg" => "/alf_images/img-file-32.png",
+    "image/jpeg2000" => "/alf_images/jpg.gif",
+    "video/mpeg" => "/alf_images/mpeg.gif",
+    "audio/x-mpeg" => "/alf_images/mpg.gif",
+    "video/mp4" => "/alf_images/mp4.gif",
+    "video/mpeg2" => "/alf_images/mp2.gif",
+    "application/pdf" => "/alf_images/pdf.gif",
+    "image/png" => "/alf_images/png.gif",
+    "application/vnd.powerpoint" => "/alf_images/ppt-file-32.png",
+    "audio/x-wav" => "/alf_images/wmv.gif",
+    "application/vnd.excel" => "/alf_images/xls-file-32.png",
+    "application/zip" => "/alf_images/zip.gif",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document" => "/alf_images/doc.gif",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" => "/alf_images/xls.gif",
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation" => "/alf_images/ppt.gif",
+    "folder" => "alf_images/generic-folder-32.png"
+  }
 
 end
